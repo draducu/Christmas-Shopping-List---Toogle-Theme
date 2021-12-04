9671ac5dc24154fb510df876355ececfc33c5f71
Raducu Daniel Teodor <raducudanielteodor@yahoo.com>
2021-12-05 00:45:03 +0200

    am prima parte, urmeaza coloana cu lista

---
+++ index.js
@@ -0,0 +1,51 @@
+const christmasBtn = document.getElementById("christmas-button")
+const treeBtn = document.getElementById("tree-button")
+const snowManBtn = document.getElementById("snow-button")
+const spanSantaEl = document.getElementById("spanSanta-el")
+const spanTreeEl = document.getElementById("spanTree-el")
+const spanSnowManEl = document.getElementById("spanSnowMan-el")
+const myImageEl = document.getElementById("myImage-el")
+
+function reveal(span) {
+    if (span.style.display == "none") {
+        span.style.display = "inline";
+    }
+}
+
+function hide(span) {
+    if (span.style.display !== "none") {
+        span.style.display = "none";
+    }
+}
+
+hide(spanTreeEl)
+hide(spanSnowManEl)
+
+christmasBtn.addEventListener("click", function() {
+    document.body.classList.remove("snow")
+    document.body.classList.add("christmas")
+    document.body.classList.remove("tree")
+    reveal(spanSantaEl)
+    hide(spanTreeEl)
+    hide(spanSnowManEl)
+    myImageEl.src ="santa.png" //mod de a seta sursa imaginii
+})
+treeBtn.addEventListener("click", function() {
+    document.body.classList.remove("snow")
+    document.body.classList.add("tree")
+    document.body.classList.remove("christmas")
+    reveal(spanTreeEl)
+    hide(spanSantaEl)
+    hide(spanSnowManEl)
+    myImageEl.setAttribute("src", "tree.png") // un mod diferit de a seta sursa imaginii
+})
+snowManBtn.addEventListener("click", function() {
+    document.body.classList.remove("christmas")
+    document.body.classList.remove("tree")
+    document.body.classList.add("snow")
+    reveal(spanSnowManEl)
+    hide(spanTreeEl)
+    hide(spanSantaEl)
+    myImageEl.src ='snowMan.png'
+})
+
---
+++ readme.md
@@ -0,0 +1,1 @@
+This project is an browser extension. Load it !
