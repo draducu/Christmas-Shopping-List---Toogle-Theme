23885e1ecf4ed9431a3cd7976d1655f7064e2388
Raducu Daniel Teodor <raducudanielteodor@yahoo.com>
2021-12-05 20:27:56 +0200

    mai am de facut lista

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const christmasBtn = document.getElementById("christmas-button")
 const treeBtn = document.getElementById("tree-button")
-const snowManBtn = document.getElementById("snow-button")
+const snowManBtn = document.getElementById("snowMan-button")
 const spanSantaEl = document.getElementById("spanSanta-el")
 const spanTreeEl = document.getElementById("spanTree-el")
 const spanSnowManEl = document.getElementById("spanSnowMan-el")
 const myImageEl = document.getElementById("myImage-el")
+let theme = ""
 
 function reveal(span) {
     if (span.style.display == "none") {
         span.style.display = "inline";
     }
 }
-
 function hide(span) {
     if (span.style.display !== "none") {
         span.style.display = "none";
@@ -25,11 +25,30 @@ else {
     console.log("not extensie")
 }
 
-hide(spanTreeEl)
-hide(spanSnowManEl)
+theme = localStorage.getItem("theme")
+console.log(theme)
 
 
-christmasBtn.addEventListener("click", function() {
+if (theme === null){
+    hide(spanTreeEl)
+    hide(spanSnowManEl)
+    document.getElementById("christmas-button").checked = true;
+}
+
+if (theme === "santa") {
+    santa()
+    document.getElementById("christmas-button").checked = true;
+}
+if (theme === "tree") {
+    tree()
+    document.getElementById("tree-button").checked = true;
+}
+if (theme === "snowMan") {
+    snowMan()
+    document.getElementById("snowMan-button").checked = true;
+}
+
+function santa() {
     document.body.classList.remove("snow")
     document.body.classList.add("christmas")
     document.body.classList.remove("tree")
@@ -37,8 +56,10 @@ christmasBtn.addEventListener("click", function() {
     hide(spanTreeEl)
     hide(spanSnowManEl)
     myImageEl.src ="pictures/santa.png" //mod de a seta sursa imaginii
-})
-treeBtn.addEventListener("click", function() {
+
+    localStorage.setItem("theme", "santa")
+}
+function tree() {
     document.body.classList.remove("snow")
     document.body.classList.add("tree")
     document.body.classList.remove("christmas")
@@ -46,14 +67,21 @@ treeBtn.addEventListener("click", function() {
     hide(spanSantaEl)
     hide(spanSnowManEl)
     myImageEl.setAttribute("src", "pictures/tree.png") // un mod diferit de a seta sursa imaginii
-})
-snowManBtn.addEventListener("click", function() {
+
+    localStorage.setItem("theme", "tree")
+}
+function snowMan() {
     document.body.classList.remove("christmas")
     document.body.classList.remove("tree")
     document.body.classList.add("snow")
     reveal(spanSnowManEl)
     hide(spanTreeEl)
     hide(spanSantaEl)
-    myImageEl.src ='pictures/snowMan.png'
-})
+    myImageEl.src ="pictures/snowMan.png"
+
+    localStorage.setItem("theme", "snowMan")
+}
+christmasBtn.addEventListener("click", santa )
+treeBtn.addEventListener("click", tree )
+snowManBtn.addEventListener("click", snowMan )
 
