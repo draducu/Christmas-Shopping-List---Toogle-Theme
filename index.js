344591822ecb17e7170d72e5ba59939ddc65cb71
Raducu Daniel Teodor <raducudanielteodor@yahoo.com>
2021-12-08 00:54:16 +0200

    am reusit sa imi construiesc html ul in javascript, urmeaza sa vad ce pot sa fac in legatura cu checkboxul, label, button si p, butonul e primul

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const spanTreeEl = document.getElementById("spanTree-el")
 const spanSnowManEl = document.getElementById("spanSnowMan-el")
 const myImageEl = document.getElementById("myImage-el")
 let theme = ""
-// let =
+
 function reveal(span) {
     if (span.style.display == "none") {
         span.style.display = "inline";
@@ -80,4 +80,86 @@ function snowMan() {
 christmasBtn.addEventListener("click", santa )
 treeBtn.addEventListener("click", tree )
 snowManBtn.addEventListener("click", snowMan )
+
+// Begin !!                             @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+let saveButtonEl = document.getElementById("save-button-el")
+let listContainerEl = document.getElementById("listContainer-el")
+let input = document.getElementById("input-text-el")
+let myList = []
+let position = -1
+class Template {
+    constructor(checkValue, textValue, button ) {
+        this.checkValue = checkValue;
+        this.textValue = textValue;
+        this.button = button;
+        this.position = position;
+    }
+}
+function renderlistItemBox() {
+    this["listItemBox" + position] = document.createElement('div')
+    this["listItemBox" + position].setAttribute("class","listItemBox")
+    listContainerEl.appendChild(this["listItemBox" + position])
+    renderlistItem()
+}
+function renderlistItem() {
+    this["listItem" + position] = document.createElement('div')
+    this["listItem" + position].setAttribute("class","listItem")
+    this["listItemBox" + position].appendChild(this["listItem" + position])
+    rendercheckBox()
+}
+function rendercheckBox() {
+    this["checkbox" + position] = document.createElement('input')
+    this["checkbox" + position].setAttribute("type","checkbox")
+    this["checkbox" + position].setAttribute("class","checkbox")
+    this["listItem" + position].appendChild(this["checkbox" + position])
+    renderlabel()
+}  
+function renderlabel() {
+    this["label" + position] = document.createElement('label')
+    this["label" + position].setAttribute("class","label")
+    this["listItem" + position].appendChild(this["label" + position])
+    this["label" + position].innerHTML = "VICTORIE"
+    renderdeleteButton()
+} 
+function renderdeleteButton() {
+    this["button" + position] = document.createElement('button')
+    this["button" + position].setAttribute("class","delete")
+    this["listItem" + position].appendChild(this["button" + position])
+    this["button" + position].innerHTML = "x"
+    renderstatus()
+} 
+function renderstatus() {
+    this["status" + position] = document.createElement('p')
+    this["status" + position].setAttribute("class","paragraph")
+    this["listItemBox" + position].appendChild(this["status" + position])
+    this["status" + position].innerHTML = "status:"
+} 
+
+
+
+
+
+
+function insert() {
+    position = position + 1
+    let myObject = new Template("checkValue", input.value, false, position)
+    
+    myList.push(myObject)
+    console.log("joined")
+    console.log(myList)
+    console.log(myList[0].textValue)
+
+    renderlistItemBox()
+}
+saveButtonEl.addEventListener("click", insert)
+
+
+
+
+
+
+
+
+
+
 
